ad7e3c0b6e8cbe759f68b6052efac5962cabfed2
rizkidarmawan21 <rizkidarmawan.0402102@gmail.com>
2021-09-13 21:45:50 +0700

    Add file

---
+++ routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FoodController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\API\MidtransController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+// Route::get('/debug-sentry', function () {
+//     throw new Exception('My first Sentry error!');
+// });y
+
+
+
+
+// Homepage
+Route::get('/', function () {
+    return redirect()->route('dashboard');
+});
+
+
+
+// dashboard
+Route::prefix('dashboard')
+    ->middleware(['auth:sanctum','admin'])
+    ->group(function() {
+            Route::get('/', [DashboardController::class, 'index'])
+            ->name('dashboard');
+            Route::resource('users', UserController::class);
+            Route::resource('food', FoodController::class);
+
+            Route::get('transactions/{id}/status/{status}', [TransactionController::class, 'changeStatus'])->name('transactions.changeStatus');
+            Route::resource('transactions', TransactionController::class);
+
+    });
+
+// midtrans related
+
+Route::get('midtrans/success',[MidtransController::class,'success']);
+Route::get('midtrans/unfinish',[MidtransController::class,'unfinish']);
+Route::get('midtrans/error',[MidtransController::class,'error']);
